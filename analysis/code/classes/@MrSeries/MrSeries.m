64f4874e60cca2e895b07cb0003dee00f60bd58e
Lars Kasper <kasper@biomed.ee.ethz.ch>
2014-07-02 07:10:22 +0000

    analysis: tested propsets for automatic Revision/id replacement in code files
    git-svn-id: http://chap04.ethz.ch/fmri_svn/development@9 af1c8887-3614-4b1b-9890-8700dbf8cd7f

--- analysis/code/classes/@MrSeries/MrSeries.m
+++ analysis/code/classes/@MrSeries/MrSeries.m
@@ -19,7 +19,7 @@ classdef MrSeries < CopyData
 % For further details, see the file COPYING or
 %  <http://www.gnu.org/licenses/>.
 %
-% $Id: new_class2.m 354 2013-12-02 22:21:41Z kasperla $
+% $Id$
 
 properties
  % COMMENT_BEFORE_PROPERTY
@@ -34,7 +34,7 @@ properties
     masks   = {}; % cell of MrImages
     rois    = {}; % cell of MrRois
     processing_log = {};
-    svn_version = '$Rev'; % code version 
+    svn_version = '$Rev$'; % code version 
     nProcessingSteps = 0;
     
     % parameters for all complicated methods
