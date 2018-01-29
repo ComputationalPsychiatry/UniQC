62589a43ff90311e750f6e1d2ae5fd30e952d7d7
Saskia Bollmann <saskia.bollmann@cai.uq.edu.au>
2018-01-29 01:52:18 +0000

    unit test: created one main file for all unit tests
    git-svn-id: http://chap04.ethz.ch/fmri_svn/development@409 af1c8887-3614-4b1b-9890-8700dbf8cd7f

---
+++ analysis/test/unit_test_main.m
@@ -0,0 +1,34 @@
+%% Run complete unit test
+% create test suite
+import matlab.unittest.TestSuite;
+% run complete test suit
+UTall = TestSuite.fromClass(?MrUnitTest);
+resultsAll = run(UTall);
+disp(table(resultsAll));
+
+%% Run test for MrDimInfo
+UTDimInfo = TestSuite.fromClass(?MrUnitTest,'Tag','MrDimInfo');
+resultsDimInfo = run(UTDimInfo);
+disp(table(resultsDimInfo));
+
+% run individuell test for MrDimInfo
+% create test object
+testCase = MrUnitTest;
+% call individual test cases
+res = run(testCase, 'MrDimInfo_get_add_remove');
+res = run(testCase, 'MrDimInfo_constructor');
+res = run(testCase, 'MrDimInfo_empty_input');
+
+%% Run test for MrAffineGeometry
+UTAffineGeometry = TestSuite.fromClass(?MrUnitTest,'Tag','MrAffineGeometry');
+resultsAffineGeometry = run(UTAffineGeometry);
+disp(table(resultsAffineGeometry));
+
+
+%% Run test for MrImageGeometry
+UTImageGeometry = TestSuite.fromClass(?MrUnitTest,'Tag','MrImageGeometry');
+resultsImageGeometry = run(UTImageGeometry);
+disp(table(resultsImageGeometry));
+
+% call individual test cases
+res = run(testCase, 'MrImageGeometry_constructor');
