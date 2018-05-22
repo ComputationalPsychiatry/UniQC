d37b518b2d591e02cfc9b9eb684648dd7db8d3ec
Lars Kasper <kasper@biomed.ee.ethz.ch>
2018-05-22 17:47:31 +0000

    first draft MrImage.smooth; wrap_spm_method not working yet
    git-svn-id: http://chap04.ethz.ch/fmri_svn/development@490 af1c8887-3614-4b1b-9890-8700dbf8cd7f

--- analysis/code/classes/@MrImage/smooth.m
+++ analysis/code/classes/@MrImage/smooth.m
@@ -1,8 +1,8 @@
-function this = smooth(this, fwhmMillimeter)
+function outputImage = smooth(this, fwhmMillimeter)
 % smoothes ND MrImage using Gaussian kernel and via SPM functionality
 %
 %   Y = MrImage()
-%   Y.smooth(inputs)
+%   Y.smooth(fwhmMillimeter)
 %
 % This is a method of class MrImage.
 %
@@ -30,39 +30,22 @@ function this = smooth(this, fwhmMillimeter)
 % $Id$
 
 %% Decide upon which type of ND image this is
-imageType = 'magnitudeND';
-
-switch imageType
-    case 'is4D'
-        thisAs4D = this.recast_as_MrImageSpm4D();
-    case 'isNd'
-        thisAs4D = this.split_into_MrImageSpm4D();
-%        combine_from_MrImageSpm4D
-        
-        
-        
-        
-       
-        
-        % cast to MrImageSpm4D, apply, cast back!
-        % constructor for MrImageSpm4D from MrImage and vice versa
-        % mrImageSpm4D = MrImageSpm4D(MrImage, convertComplexMode)
-        % convertComplexMode = {'re/im', 'abs/phase'}
-    case 'complex'
-        % split into extra dimension magnitude/phase separately and smooth
-        % then
-        
-        % or real and imaginary? ... warning?
-    case 'magnitudeND' % all magnitude, but more than 4 non-singleton dimensions
-        % x,y,z,t are smoothed as 4D volumes separarately, split along all
-        % other dimensions
-        this.wrap_spm_method(@smooth);
+if nargin < 2
+    fwhmMillimeter = 8;
 end
 
+isComplexImage = ~isreal(this);
 
-%% split (and recast) into applicable 4D-SPM images
-        % smooth
+if isComplexImage
+    outputImage = this.split_complex('mp');
+else
+    outputImage = this.copyobj();
+end
 
-%% smooth all MrImageSpm4D entities
+outputImage = outputImage.wrap_spm_method(@smooth, ...
+    {fwhmMillimeter});
 
-%% reassemble smoothed images into one again
+%% reassemble complex smoothed images into one again
+if isComplexImage
+  outputImage = outputImage.combine_complex();
+end
