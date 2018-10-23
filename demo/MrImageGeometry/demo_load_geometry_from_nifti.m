da29eec957e4db671f67356c981347a740a9c85e
Saskia <saskia.bollmann@cai.uq.edu.au>
2018-10-23 14:17:58 +1000

    Added an example for shifting the sampling points of dimInfo, such that [0 0 0] is in the centre of the matrix.

--- demo/MrImageGeometry/demo_load_geometry_from_nifti.m
+++ demo/MrImageGeometry/demo_load_geometry_from_nifti.m
@@ -29,7 +29,22 @@ geometry.isequal(geometry3);
 %% Shift origin
 % shifts origin of dimInfo into center of data block, puts scaling of
 % affineGeom to 1, since it is reflected by dimInfo.resolutions
+origAffineGeom = geometry.get_affine_matrix;
+dimIndex = [dimInfo.get_dim_index('x'), dimInfo.get_dim_index('y'), dimInfo.get_dim_index('z')];
+nSamples = dimInfo.nSamples(dimIndex);
+
+dimInfo.set_dims(dimIndex, 'arrayIndex', {1:nSamples(1), 1:nSamples(2), 1:nSamples(3)}, ...
+    'samplingPoints', cellfun(@minus, dimInfo.samplingPoints(dimIndex), ...
+    num2cell(geometry.FOV_mm/2), 'UniformOutput', 0));
+
+ADimInfo = dimInfo.get_affine_matrix;
+affineGeometry.update_from_affine_matrix(origAffineGeom/ADimInfo);
+geometry4 = MrImageGeometry(affineGeometry, dimInfo);
+geometry.isequal(geometry4)
+
+
 [this.dimInfo,this.affineGeometry] = geometry.convert_nifti_to_uniqc();
+
 
 image.geometry -> set method of MrImage -> 
             MrImageGeometry(this.dimInfo, this.affineGeometry)
