e70e7099a85df74e4eb7c7ee5993c2f3c060203e
Lars Kasper <kasper@biomed.ee.ethz.ch>
2019-03-29 16:06:21 +0100

    investigating Weisskoff plot; smoothing ~= noise correlation (since noise amplitude also changes by averaging!)

--- demo/Paper/Weisskoff/sim_Weisskoff.m
+++ demo/Paper/Weisskoff/sim_Weisskoff.m
@@ -1,12 +1,16 @@
-clear; close all; clc;
-% generate image with random noise (0.1%)
-m = MrImage(rand(51,51,5,200)+1000);
+% clear; close all; clc;
+% generate image with random noise (1%)
+m = MrImage(randn(51,51,5,200)+1);
+
+doPlotRoi = false;
 
 % make ROIs
 for n = 1:20
     m_roi{n} = MrImage(zeros(m.dimInfo.nSamples(1:3)));
-    m_roi{n}.data(27-n:25+n, 27-n:25+n, 2:4) = 1;
-    m_roi{n}.plot;
+    m_roi{n}.data(27-n:25+n, 27-n:25+n, 3) = 1;
+    if doPlotRoi
+        m_roi{n}.plot;
+    end
 end
 
 % extract data
@@ -20,8 +24,12 @@ for n = 1:40
     CV(n) = std(m.rois{n}.perVolume.mean)/mean(m.rois{n}.perVolume.mean);
 end
 
-% plot results
+%% plot results
 figure; plot(1:20, CV(1:20), 1:20, CV(21:40));
 legend('random', 'smoothed');
-fh = figure; plot(log10(1:20), log10(CV(1:20)), log10(1:20), log10(CV(21:40)));
+fh = figure; plot(log10(1:20), log10(100*CV(1:20)), log10(1:20), log10(100*CV(21:40)));
+legend('random', 'smoothed');
+fh = figure; loglog(1:20, 100*CV(1:20), 1:20, 100*CV(21:40));
+fh.CurrentAxes.XTick = [1 10 100];
+fh.CurrentAxes.YTick = [0.01 0.1 1 10];
 legend('random', 'smoothed');
