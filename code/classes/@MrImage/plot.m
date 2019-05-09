7488b83a2ba2af613b8e836291855b650b950ced
Lars Kasper <kasper@biomed.ee.ethz.ch>
2019-05-09 19:23:01 +0200

    bugfix 3D images with selection Indices undefined in plot

--- code/classes/@MrImage/plot.m
+++ code/classes/@MrImage/plot.m
@@ -277,9 +277,8 @@ if isPlotDataSpecified
     plotDataSpecified = repmat(plotDataSpecified, 2, 1);
     plotDataSpecified = reshape(plotDataSpecified, 1, []);
     stringSelection = varargin(plotDataSpecified);
-    [plotImage, selectionIndexArray, ~] = plotImage.select('type', selectionType, ...
-        stringSelection{:});
 else
+    stringSelection = {};
     if ~useSlider % default: no slider used
         % 1 image with all samples of first three dimensions, for all further
         % dimensions only first sample is plotted
@@ -288,8 +287,6 @@ else
             dimLabelsSelect = plotImage.dimInfo.dimLabels;
             stringSelection(1:2:nDimsSelect*2) = dimLabelsSelect(4:end);
             stringSelection(2:2:nDimsSelect*2) = {1};
-            [plotImage, selectionIndexArray] = plotImage.select('type', selectionType, ...
-                stringSelection{:});
         end
     else % use slider
         % 1 image with all samples of first FOUR dimensions, for all further
@@ -299,11 +296,12 @@ else
             dimLabelsSelect = plotImage.dimInfo.dimLabels;
             stringSelection(1:2:nDimsSelect*2) = dimLabelsSelect(5:end);
             stringSelection(2:2:nDimsSelect*2) = {1};
-            [plotImage, selectionIndexArray] = plotImage.select('type', selectionType, ...
-                stringSelection{:});
         end
         
     end
+    [plotImage, selectionIndexArray] = plotImage.select('type', selectionType, ...
+        stringSelection{:});
+    
 end
 
 
