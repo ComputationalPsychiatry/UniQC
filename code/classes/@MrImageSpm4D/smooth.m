809561399bddbfe23863cb9a742a7801917f5343
Saskia <saskia.bollmann@cai.uq.edu.au>
2019-11-27 14:04:18 -0500

    MrImageSpm4D.smooth now returns an new MrImageSpm4D object.

--- code/classes/@MrImageSpm4D/smooth.m
+++ code/classes/@MrImageSpm4D/smooth.m
@@ -1,46 +1,56 @@
-function this = smooth(this, fwhmMillimeter)
-% Smoothes image (or image time series) spatially with Gaussian kernel
+function outputImage = smooth(this, varargin)
+% Smoothes image (or image time series) spatially with sinc-truncated Gaussian kernel, 
+% i.e., using spm_smooth and mimicking its functionality
 %
 %   Y = MrImageSpm4D()
-%   Y = Y.smooth(fwhmMillimeter)
+%   sY = Y.smooth('fwhm', fwhm)
 %
-% This is a method of class MrImageSpm4D.
+% outputImage is a method of class MrImageSpm4D.
+% 
+% NOTE: For complex-valued data, use MrImage.smooth
 %
 % IN
-%   fwhmMillimeter  Full width at half maximum of Gaussian kernel (in mm)
+%   fwhm    [1,1] or [1,3] Full width at half maximum of Gaussian kernel (in mm)
+%           if single value is given, isotropic kernel is assumed
+%           default: 8
+%
 % OUT
 %
 % EXAMPLE
 %   smooth
 %
-%   See also MrImageSpm4D
+%   See also MrImageSpm4D MrImage.smooth spm_smooth
 
 % Author:   Saskia Klein & Lars Kasper
 % Created:  2014-07-02
 % Copyright (C) 2014 Institute for Biomedical Engineering
 %                    University of Zurich and ETH Zurich
 %
-% This file is part of the TAPAS UniQC Toolbox, which is released
+% outputImage file is part of the TAPAS UniQC Toolbox, which is released
 % under the terms of the GNU General Public Licence (GPL), version 3. 
 % You can redistribute it and/or modify it under the terms of the GPL
 % (either version 3 or, at your option, any later version).
 % For further details, see the file COPYING or
 %  <http://www.gnu.org/licenses/>.
 
+defaults.fwhm = 8;
 
-if length(fwhmMillimeter) == 1
-    fwhmMillimeter = fwhmMillimeter*[ 1 1 1];
+args = propval(varargin, defaults);
+strip_fields(args);
+
+if length(fwhm) == 1
+    fwhm = fwhm*[ 1 1 1];
 end
 
+outputImage = this.copyobj;
 
 % save image file for processing as nii in SPM
-this.save('fileName', this.get_filename('raw'));
+outputImage.save('fileName', outputImage.get_filename('raw'));
 
-matlabbatch = this.get_matlabbatch('smooth', fwhmMillimeter);
-save(fullfile(this.parameters.save.path, 'matlabbatch.mat'), ...
+matlabbatch = outputImage.get_matlabbatch('smooth', fwhm);
+save(fullfile(outputImage.parameters.save.path, 'matlabbatch.mat'), ...
             'matlabbatch');
 spm_jobman('run', matlabbatch);
 
 % clean up: move/delete processed spm files, load new data into matrix
-
-this.finish_processing_step('smooth');
+outputImage.finish_processing_step('smooth');
