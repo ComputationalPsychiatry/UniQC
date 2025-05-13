77e844c9dd4692631648c1554c69f52042f2d60d
Saskia Bollmann <saskia.bollmann@cai.uq.edu.au>
2025-05-13 14:24:53 +1000

    Im;lemented first version of the T2* fitting, but nans and large, negative T2* values a problem

--- demo/Paper/tapas_uniqc_Reddy_ME_example.m
+++ demo/Paper/tapas_uniqc_Reddy_ME_example.m
@@ -92,19 +92,72 @@ rData.parameters.save.path = 'C:\Users\uqsboll2\Desktop\Reddy_ME_results';
 rData.parameters.save.fileName = [strrep(rawMeFilename, 'echo-5_', ''), '.nii'];
 disp(['Saving ', rData.get_filename]);
 rData.save();
+% for loading, use rData = MrImage('C:\Users\uqsboll2\Desktop\Reddy_ME_results')
 
 % estimate T2*-based weights based on Poser et al., MRM, 2006 using a
 % general linear model
-meanRData = rData.mean('t');
-meanRData.plot('rotate90', 1, 'echoTime', 1:meanRData.dimInfo.nSamples('echoTime'));
+% created using chatgpt
 
-% linearise across echoes using the natural logarithm
-logMeanRData = meanRData.log(); 
-logMeanRData.plot('rotate90', 1, 'echoTime', 1:logMeanRData.dimInfo.nSamples('echoTime'));
-
-% extract TEs as vector
+% extract TEs
 TE = rData.dimInfo.samplingPoints{'echoTime'};
 
+% Ensure TE is a column vector
+TE = TE(:);
+nTE = length(TE);
+
+% compute mean across time
+meanRData = rData.mean('t');
+meanRData.plot('rotate90', 1, 'echoTime', 1:meanRData.dimInfo.nSamples('echoTime'));
+% remove unnecessary dimensions
+meanRData = meanRData.remove_dims('t');
+
+% Get size of input
+S_size = size(meanRData.data);
+spatialSize = S_size(1:end-1);
+nVoxels = prod(spatialSize);
+
+% Reshape to [nTE x nVoxels]
+S_reshaped = reshape(meanRData.data, [nVoxels, nTE])';  % [nTE x nVoxels]
+
+% Mask: only use voxels where all values > 0
+validMask = all(S_reshaped > 0, 1);
+
+% Log-transform valid signals
+logS = zeros(size(S_reshaped));
+logS(:, validMask) = log(S_reshaped(:, validMask));
+
+% Design matrix and pseudoinverse
+X = [ones(nTE, 1), TE];     % [nTE x 2]
+X_pinv = pinv(X);           % [2 x nTE]
+
+% Solve for beta = [ln(S0); -1/T2]
+beta = X_pinv * logS(:, validMask);  % [2 x nValidVoxels]
+lnS0_valid = beta(1, :);
+negInvT2_valid = beta(2, :);
+
+% Compute T2 and S0
+S0_valid = exp(lnS0_valid);
+T2_valid = -1 ./ negInvT2_valid;
+
+% Initialize maps
+T2mapData = NaN(nVoxels, 1);
+S0mapData = NaN(nVoxels, 1);
+
+T2mapData(validMask) = T2_valid;
+S0mapData(validMask) = S0_valid;
+
+% Reshape back to original spatial dimensions
+T2mapData = reshape(T2mapData, spatialSize);
+S0mapData = reshape(S0mapData, spatialSize);
+
+% recast as MrImages
+T2map = meanRData.copyobj();
+T2map = T2map.remove_dims('echoTime');
+T2map.data = T2mapData;
+
+S0 = meanRData.copyobj();
+S0 = S0.remove_dims('echoTime');
+S0.data = S0mapData;
 
 
 
